a4cf6b97ed39d2d6d3b9cbdac8705151fc1f79aa
T. G. <6294097+tomgk@users.noreply.github.com>
2025-03-19 20:53:57 +0100

    add FACTOR

--- city-sim/src/main/java/org/exolin/citysim/GamePanel.java
+++ city-sim/src/main/java/org/exolin/citysim/GamePanel.java
@@ -2,6 +2,7 @@ package org.exolin.citysim;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import javax.swing.JComponent;
 
 /**
@@ -11,20 +12,20 @@ import javax.swing.JComponent;
 public class GamePanel extends JComponent
 {
     private final int size = 100;
+    private static final int FACTOR = 2;
     
     @Override
     public void paint(Graphics g)
     {
         g.drawRect(0, 0, getWidth()-1, getHeight()-1);
         
-        draw((Graphics2D)g, Math.min(getWidth(), getHeight()*2));
+        draw((Graphics2D)g, Math.min(getWidth(), getHeight()*FACTOR));
     }
     
     private void draw(Graphics2D g, int dim)
     {
         /*
-        double w = (double)width / size;
-        double h = (double)height / size;
+        double s = (double)dim / size;
         
         for(int i = 0; i<size;++i)
         {
@@ -32,9 +33,9 @@ public class GamePanel extends JComponent
         }
         */
         
-        g.drawLine(0, dim/2/2, dim/2, 0);
-        g.drawLine(dim/2, 0, dim, dim/2/2);
-        g.drawLine(dim, dim/2/2, dim/2, dim/2);
-        g.drawLine(dim/2, dim/2, 0, dim/2/2);
+        g.drawLine(0, dim/FACTOR/2, dim/2, 0);
+        g.drawLine(dim/2, 0, dim, dim/FACTOR/2);
+        g.drawLine(dim, dim/FACTOR/2, dim/2, dim/FACTOR);
+        g.drawLine(dim/2, dim/FACTOR, 0, dim/FACTOR/2);
     }
 }

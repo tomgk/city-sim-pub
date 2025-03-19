f8172045d7751aae2548c39df1b4d2986f4e934a
T. G. <6294097+tomgk@users.noreply.github.com>
2025-03-19 20:48:59 +0100

    keep X/Y relation constant

--- city-sim/src/main/java/org/exolin/citysim/GamePanel.java
+++ city-sim/src/main/java/org/exolin/citysim/GamePanel.java
@@ -10,16 +10,31 @@ import javax.swing.JComponent;
  */
 public class GamePanel extends JComponent
 {
+    private final int size = 100;
+    
     @Override
     public void paint(Graphics g)
     {
         g.drawRect(0, 0, getWidth()-1, getHeight()-1);
         
-        draw((Graphics2D)g, getWidth(), getHeight());
+        draw((Graphics2D)g, Math.min(getWidth(), getHeight()*2));
     }
     
-    private void draw(Graphics2D g, int width, int height)
+    private void draw(Graphics2D g, int dim)
     {
+        int width = dim;
+        int height = dim/2;
+        
+        /*
+        double w = (double)width / size;
+        double h = (double)height / size;
+        
+        for(int i = 0; i<size;++i)
+        {
+            g.drawLine(0, height/2, width/2, 0);
+        }
+        */
+        
         g.drawLine(0, height/2, width/2, 0);
         g.drawLine(width/2, 0, width, height/2);
         g.drawLine(width, height/2, width/2, height);

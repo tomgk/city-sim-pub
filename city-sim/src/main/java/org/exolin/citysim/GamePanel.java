31e732d622a39efeef1db55a50202b4a5a826486
T. G. <6294097+tomgk@users.noreply.github.com>
2025-03-19 20:42:24 +0100

    draw outline of border

--- city-sim/src/main/java/org/exolin/citysim/GamePanel.java
+++ city-sim/src/main/java/org/exolin/citysim/GamePanel.java
@@ -1,6 +1,7 @@
 package org.exolin.citysim;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JComponent;
 
 /**
@@ -13,5 +14,15 @@ public class GamePanel extends JComponent
     public void paint(Graphics g)
     {
         g.drawRect(0, 0, getWidth()-1, getHeight()-1);
+        
+        draw((Graphics2D)g, getWidth(), getHeight());
+    }
+    
+    private void draw(Graphics2D g, int width, int height)
+    {
+        g.drawLine(0, height/2, width/2, 0);
+        g.drawLine(width/2, 0, width, height/2);
+        g.drawLine(width, height/2, width/2, height);
+        g.drawLine(width/2, height, 0, height/2);
     }
 }

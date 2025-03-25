9dc34069b5ef66ca869c0b65e641d3ad3c9e1932
T. G. <6294097+tomgk@users.noreply.github.com>
2025-03-25 20:27:10 +0100

    test fix for #13

--- city-sim/src/test/java/org/exolin/citysim/storage/WorldStorageTest.java
+++ city-sim/src/test/java/org/exolin/citysim/storage/WorldStorageTest.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.exolin.citysim.ActualBuilding;
+import org.exolin.citysim.ActualBuildingType;
 import org.exolin.citysim.World;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,7 +19,7 @@ public class WorldStorageTest
     public void testSerializeActualBuilding() throws IOException
     {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        WorldStorage.serialize(new ActualBuilding(World.cinema, 16, 99, 0), out);
+        WorldStorage.serialize(new ActualBuilding(World.cinema, 16, 99, ActualBuildingType.Variant.DEFAULT), out);
         String output = out.toString(StandardCharsets.UTF_8);
         String expected = "{\"type\":\"cinema\",\"x\":16,\"y\":99,\"variant\":0}";
         Assertions.assertEquals(expected, output);

cc97a1f39fc35114e45d1fa60ce68a4023273275
T. G. <6294097+tomgk@users.noreply.github.com>
2025-03-30 19:37:19 +0200

    Create world2.cs
    part of #9

---
+++ city-sim/saves/world2.cs
@@ -0,0 +1,173 @@
+{
+  "gridSize" : 30,
+  "buildings" : [ {
+    "type" : "street",
+    "x" : 2,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 3,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 4,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "office_3",
+    "x" : 6,
+    "y" : 2
+  }, {
+    "type" : "street",
+    "x" : 5,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "parkbuilding",
+    "x" : 3,
+    "y" : 6
+  }, {
+    "type" : "street",
+    "x" : 6,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 7,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 8,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 9,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "office",
+    "x" : 6,
+    "y" : 6
+  }, {
+    "type" : "street",
+    "x" : 10,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 11,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 12,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 13,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 14,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "cinema",
+    "x" : 16,
+    "y" : 2
+  }, {
+    "type" : "street",
+    "x" : 15,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 16,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 17,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "office_2",
+    "x" : 19,
+    "y" : 2
+  }, {
+    "type" : "street",
+    "x" : 18,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 19,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "car-cinema",
+    "x" : 16,
+    "y" : 6
+  }, {
+    "type" : "street",
+    "x" : 20,
+    "y" : 5,
+    "variant" : "t_intersection_2"
+  }, {
+    "type" : "street",
+    "x" : 21,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 20,
+    "y" : 6,
+    "variant" : "connect_y"
+  }, {
+    "type" : "street",
+    "x" : 22,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 20,
+    "y" : 7,
+    "variant" : "connect_y"
+  }, {
+    "type" : "street",
+    "x" : 23,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 20,
+    "y" : 8,
+    "variant" : "connect_y"
+  }, {
+    "type" : "street",
+    "x" : 24,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 20,
+    "y" : 9,
+    "variant" : "connect_y"
+  }, {
+    "type" : "street",
+    "x" : 25,
+    "y" : 5,
+    "variant" : "connect_x"
+  }, {
+    "type" : "street",
+    "x" : 26,
+    "y" : 5,
+    "variant" : "connect_x"
+  } ]
+}

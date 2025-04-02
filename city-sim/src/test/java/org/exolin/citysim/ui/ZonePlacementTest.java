b0d2cff290bc977fe6af8ba0a64aac6d93b473e1
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-02 23:08:49 +0200

    improve on #35

--- city-sim/src/test/java/org/exolin/citysim/ui/ZonePlacementTest.java
+++ city-sim/src/test/java/org/exolin/citysim/ui/ZonePlacementTest.java
@@ -19,13 +19,39 @@ public class ZonePlacementTest
 {
     private void assertZone(Building b, int x, int y, ZoneType t)
     {
-        assertEquals(x, b.getX(), x);
-        assertEquals(y, b.getY(), y);
+        String msg = x+"/"+y;
+        assertEquals(x, b.getX(), msg+" - x");
+        assertEquals(y, b.getY());
         assertEquals(t, b.getType());
     }
     
     @Test
     public void testZonePlacementOnEmpty()
+    {
+        World world = new World(100);
+        
+        ZonePlacement z = new ZonePlacement(() -> world, Zones.zone_business);
+        
+        z.mouseDown(new Point(1, 6));
+        z.moveMouse(new Point(3, 9));
+        z.releaseMouse(new Point(3, 9));
+        
+        List<Building> buildings = world.getBuildings();
+        assertEquals(6, world.getBuildings().size(), buildings.toString());
+        
+        for(Building b: buildings)
+            System.out.println(b);
+        
+        assertZone(world.getBuildingAt(1, 6), 1, 6, Zones.zone_business);
+        assertZone(world.getBuildingAt(1, 7), 1, 7, Zones.zone_business);
+        assertZone(world.getBuildingAt(1, 8), 1, 8, Zones.zone_business);
+        assertZone(world.getBuildingAt(2, 6), 2, 6, Zones.zone_business);
+        assertZone(world.getBuildingAt(2, 7), 2, 7, Zones.zone_business);
+        assertZone(world.getBuildingAt(2, 8), 2, 8, Zones.zone_business);
+    }
+    /*
+    @Test
+    public void testZoneDisplacement()
     {
         World world = new World(100);
         
@@ -40,5 +66,5 @@ public class ZonePlacementTest
         
         assertZone(buildings.get(0), 1, 6, Zones.zone_business);
         assertZone(buildings.get(0), 2, 6, Zones.zone_business);
-    }
+    }*/
 }

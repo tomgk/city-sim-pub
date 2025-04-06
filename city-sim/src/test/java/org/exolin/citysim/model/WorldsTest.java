012256911e7d32965a4146b26cf9c6c1dd539282
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-07 01:13:38 +0200

    add tests

---
+++ city-sim/src/test/java/org/exolin/citysim/model/WorldsTest.java
@@ -0,0 +1,22 @@
+package org.exolin.citysim.model;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Thomas
+ */
+public class WorldsTest
+{
+    @Test
+    public void world1()
+    {
+        Worlds.World1();
+    }
+    
+    @Test
+    public void world2()
+    {
+        Worlds.World2();
+    }
+}

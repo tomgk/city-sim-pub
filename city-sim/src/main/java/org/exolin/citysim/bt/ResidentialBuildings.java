f1223da61d6d6fcc63fee3a5e0fe4ce5ef9d06e5
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-06 23:22:28 +0200

    add more residential buildings

--- city-sim/src/main/java/org/exolin/citysim/bt/ResidentialBuildings.java
+++ city-sim/src/main/java/org/exolin/citysim/bt/ResidentialBuildings.java
@@ -10,6 +10,17 @@ import org.exolin.citysim.model.ActualBuildingType;
 public class ResidentialBuildings
 {
     public static final ActualBuildingType small_house_1 = createBuildingType("small_house_1", 1, Zones.zone_residential);
+    
+    public static final ActualBuildingType small_houses_1 = createBuildingType("small_houses_1", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_2 = createBuildingType("small_houses_2", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_3 = createBuildingType("small_houses_3", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_4 = createBuildingType("small_houses_4", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_5 = createBuildingType("small_houses_5", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_6 = createBuildingType("small_houses_6", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_7 = createBuildingType("small_houses_7", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_8 = createBuildingType("small_houses_8", 1, Zones.zone_residential);
+    public static final ActualBuildingType small_houses_9 = createBuildingType("small_houses_9", 1, Zones.zone_residential);
+    
     public static final ActualBuildingType house_1 = createBuildingType("house_1", 3, Zones.zone_residential);
     public static final ActualBuildingType house_2 = createBuildingType("house_2", 3, Zones.zone_residential);
     public static final ActualBuildingType house_3 = createBuildingType("house_3", 4, Zones.zone_residential);

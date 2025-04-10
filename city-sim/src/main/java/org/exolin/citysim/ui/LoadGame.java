efaa5a77de9f643dc2d15e83404e77c2a90cd4ea
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-10 23:32:58 +0200

    more for #62

--- city-sim/src/main/java/org/exolin/citysim/ui/LoadGame.java
+++ city-sim/src/main/java/org/exolin/citysim/ui/LoadGame.java
@@ -1,9 +1,15 @@
 package org.exolin.citysim.ui;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import org.exolin.citysim.model.World;
 
 /**
@@ -12,12 +18,48 @@ import org.exolin.citysim.model.World;
  */
 public class LoadGame extends JFrame
 {
-    public LoadGame(List<World> worlds)
+    private final List<World> worlds;
+    private final JList list;
+    private final Consumer<World> choice;
+    
+    public LoadGame(List<World> worlds, Consumer<World> choice)
     {
+        this.worlds = Objects.requireNonNull(worlds);
+        this.choice = Objects.requireNonNull(choice);
         setTitle("Select City");
         setLayout(new BorderLayout());
-        add(new JList(worlds.stream().map(World::getName).toArray()));
+        
+        list = new JList(worlds.stream().map(World::getName).toArray());
+        add(list, BorderLayout.CENTER);
+        
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1, 0));
+        
+        JButton choose = new JButton("OK");
+        buttonPanel.add(choose);
+        choose.addActionListener(a -> {
+            executeSelection();
+        });
+        
+        JButton close = new JButton("Exit");
+        buttonPanel.add(close);
+        close.addActionListener(a->setVisible(false));
+        
+        add(buttonPanel, BorderLayout.SOUTH);
+        
         pack();
         setLocationRelativeTo(null);
+    }
+    
+    private void executeSelection()
+    {
+        int index = list.getSelectedIndex();
+        if(index == -1)
+            return;
+
+        World w = worlds.get(index);
+
+        setVisible(false);
+        choice.accept(w);
     }
 }

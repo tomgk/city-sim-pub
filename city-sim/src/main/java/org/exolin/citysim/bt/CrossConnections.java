1c5f6e79e4a53742dac07fcf8c7e0a3337e67286
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-13 22:34:07 +0200

    more for #65

--- city-sim/src/main/java/org/exolin/citysim/bt/CrossConnections.java
+++ city-sim/src/main/java/org/exolin/citysim/bt/CrossConnections.java
@@ -1,5 +1,7 @@
 package org.exolin.citysim.bt;
 
+import static org.exolin.citysim.bt.Streets.rail;
+import static org.exolin.citysim.bt.Streets.street;
 import org.exolin.citysim.model.Animation;
 import org.exolin.citysim.model.street.StreetType;
 import org.exolin.citysim.model.street.cross.CrossConnectionType;
@@ -10,8 +12,13 @@ import org.exolin.citysim.model.street.cross.CrossConnectionType;
  */
 public class CrossConnections
 {
-    public static CrossConnectionType createCrossConnectionType(StreetType type1, StreetType type2, String name)
+    public static CrossConnectionType STREET_RAIL = createCrossConnectionType(street, rail);
+    public static CrossConnectionType RAIL_STREET = createCrossConnectionType(rail, street);
+    
+    public static CrossConnectionType createCrossConnectionType(StreetType type1, StreetType type2)
     {
+        String name = type1.getName()+"_"+type2.getName();
+        System.out.println(name);
         return new CrossConnectionType(name, Animation.createUnanimated("cross_connection/"+name), 1, type1, type2);
     }
 }

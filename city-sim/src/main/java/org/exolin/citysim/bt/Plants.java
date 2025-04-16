60ebac3516aca551bb31998729e6699a9ab1bbd5
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-16 21:01:11 +0200

    move plant pics into subdir

--- city-sim/src/main/java/org/exolin/citysim/bt/Plants.java
+++ city-sim/src/main/java/org/exolin/citysim/bt/Plants.java
@@ -11,7 +11,7 @@ import org.exolin.citysim.model.ab.ActualBuildingType;
  */
 public class Plants
 {
-    public static final ActualBuildingType plant_solar = createBuildingType(createUnanimated("plant_solar"), 4, Zones.plants, 1300);
-    public static final ActualBuildingType gas_plant = createBuildingType(createAnimation("gas_plant", 8), 4, Zones.plants, 2000);
-    public static final ActualBuildingType oil_plant = createBuildingType(createAnimation("oil_plant", 8), 4, Zones.plants, 6600);
+    public static final ActualBuildingType plant_solar = createBuildingType(createUnanimated("plants/plant_solar"), 4, Zones.plants, 1300);
+    public static final ActualBuildingType gas_plant = createBuildingType(createAnimation("plants/gas_plant", 8), 4, Zones.plants, 2000);
+    public static final ActualBuildingType oil_plant = createBuildingType(createAnimation("plants/oil_plant", 8), 4, Zones.plants, 6600);
 }

2b2505081e2516569a9b89e94fc0ba79e31d10b4
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-16 19:50:42 +0200

    Create trees.cs

---
+++ city-sim/saves/trees.cs
@@ -0,0 +1,898 @@
+{
+  "gridSize" : 30,
+  "money" : 98401,
+  "buildings" : [ {
+    "type" : "trees_1",
+    "x" : 7,
+    "y" : 5,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 6,
+    "y" : 6,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 5,
+    "y" : 7,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 8,
+    "y" : 5,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 7,
+    "y" : 6,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 6,
+    "y" : 7
+  }, {
+    "type" : "trees_3",
+    "x" : 5,
+    "y" : 8,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 9,
+    "y" : 5,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 8,
+    "y" : 6,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 7,
+    "y" : 7,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 5,
+    "y" : 9,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 6,
+    "y" : 8,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 6,
+    "y" : 9,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 5,
+    "y" : 10,
+    "variant" : "left"
+  }, {
+    "type" : "trees_3",
+    "x" : 9,
+    "y" : 6,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 7,
+    "y" : 8,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 8,
+    "y" : 7,
+    "variant" : "left"
+  }, {
+    "type" : "trees_2",
+    "x" : 10,
+    "y" : 5,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 7,
+    "y" : 9,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 6,
+    "y" : 10,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 5,
+    "y" : 11,
+    "variant" : "right"
+  }, {
+    "type" : "trees_3",
+    "x" : 9,
+    "y" : 7,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_3",
+    "x" : 10,
+    "y" : 6,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_3",
+    "x" : 8,
+    "y" : 8,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 11,
+    "y" : 5,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 10,
+    "y" : 7,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 7,
+    "y" : 10,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_4",
+    "x" : 9,
+    "y" : 8,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 6,
+    "y" : 11
+  }, {
+    "type" : "trees_1",
+    "x" : 5,
+    "y" : 12,
+    "variant" : "right"
+  }, {
+    "type" : "trees_3",
+    "x" : 11,
+    "y" : 6,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_5",
+    "x" : 8,
+    "y" : 9,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 12,
+    "y" : 5,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 7,
+    "y" : 11,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_6",
+    "x" : 10,
+    "y" : 8,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 6,
+    "y" : 12,
+    "variant" : "right"
+  }, {
+    "type" : "trees_4",
+    "x" : 9,
+    "y" : 9,
+    "variant" : "right"
+  }, {
+    "type" : "trees_3",
+    "x" : 11,
+    "y" : 7,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 8,
+    "y" : 10,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 12,
+    "y" : 6
+  }, {
+    "type" : "trees_6",
+    "x" : 11,
+    "y" : 8,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 6,
+    "y" : 13,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_5",
+    "x" : 10,
+    "y" : 9,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 9,
+    "y" : 10,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_4",
+    "x" : 12,
+    "y" : 7
+  }, {
+    "type" : "trees_4",
+    "x" : 8,
+    "y" : 11,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 13,
+    "y" : 6,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 13,
+    "y" : 7
+  }, {
+    "type" : "trees_3",
+    "x" : 8,
+    "y" : 12
+  }, {
+    "type" : "trees_5",
+    "x" : 11,
+    "y" : 9,
+    "variant" : "left"
+  }, {
+    "type" : "trees_6",
+    "x" : 10,
+    "y" : 10,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 12,
+    "y" : 8,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 7,
+    "y" : 13,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 6,
+    "y" : 14,
+    "variant" : "right"
+  }, {
+    "type" : "trees_3",
+    "x" : 9,
+    "y" : 11,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_3",
+    "x" : 14,
+    "y" : 6,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 15,
+    "y" : 6,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_6",
+    "x" : 11,
+    "y" : 10,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 8,
+    "y" : 13,
+    "variant" : "left"
+  }, {
+    "type" : "trees_3",
+    "x" : 14,
+    "y" : 7,
+    "variant" : "left"
+  }, {
+    "type" : "trees_2",
+    "x" : 7,
+    "y" : 14
+  }, {
+    "type" : "trees_5",
+    "x" : 12,
+    "y" : 9,
+    "variant" : "left"
+  }, {
+    "type" : "trees_6",
+    "x" : 10,
+    "y" : 11,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_6",
+    "x" : 13,
+    "y" : 8,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 9,
+    "y" : 12,
+    "variant" : "right"
+  }, {
+    "type" : "trees_6",
+    "x" : 11,
+    "y" : 11,
+    "variant" : "left"
+  }, {
+    "type" : "trees_4",
+    "x" : 14,
+    "y" : 8,
+    "variant" : "right"
+  }, {
+    "type" : "trees_3",
+    "x" : 9,
+    "y" : 13,
+    "variant" : "left"
+  }, {
+    "type" : "trees_6",
+    "x" : 12,
+    "y" : 10,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 8,
+    "y" : 14,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 15,
+    "y" : 7,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 7,
+    "y" : 15,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_6",
+    "x" : 13,
+    "y" : 9,
+    "variant" : "right"
+  }, {
+    "type" : "trees_5",
+    "x" : 10,
+    "y" : 12,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 16,
+    "y" : 7,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 11,
+    "y" : 12,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 9,
+    "y" : 14,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_6",
+    "x" : 14,
+    "y" : 9,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 12,
+    "y" : 11,
+    "variant" : "left"
+  }, {
+    "type" : "trees_2",
+    "x" : 13,
+    "y" : 10,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_4",
+    "x" : 15,
+    "y" : 8,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 8,
+    "y" : 15,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_3",
+    "x" : 10,
+    "y" : 13,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 10,
+    "y" : 14,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_5",
+    "x" : 15,
+    "y" : 9
+  }, {
+    "type" : "trees_6",
+    "x" : 12,
+    "y" : 12,
+    "variant" : "right"
+  }, {
+    "type" : "trees_6",
+    "x" : 13,
+    "y" : 11
+  }, {
+    "type" : "trees_2",
+    "x" : 9,
+    "y" : 15,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 8,
+    "y" : 16,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 14,
+    "y" : 10,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_6",
+    "x" : 11,
+    "y" : 13,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_4",
+    "x" : 16,
+    "y" : 8,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 15,
+    "y" : 10,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 10,
+    "y" : 15,
+    "variant" : "left"
+  }, {
+    "type" : "trees_6",
+    "x" : 12,
+    "y" : 13,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_6",
+    "x" : 13,
+    "y" : 12,
+    "variant" : "right"
+  }, {
+    "type" : "trees_5",
+    "x" : 14,
+    "y" : 11,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 9,
+    "y" : 16,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 8,
+    "y" : 17,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_5",
+    "x" : 11,
+    "y" : 14,
+    "variant" : "right"
+  }, {
+    "type" : "trees_4",
+    "x" : 16,
+    "y" : 9,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 11,
+    "y" : 15,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_5",
+    "x" : 16,
+    "y" : 10
+  }, {
+    "type" : "trees_6",
+    "x" : 14,
+    "y" : 12,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_6",
+    "x" : 15,
+    "y" : 11,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 13,
+    "y" : 13,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 10,
+    "y" : 16,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 9,
+    "y" : 17,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_5",
+    "x" : 12,
+    "y" : 14,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_3",
+    "x" : 17,
+    "y" : 9,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 16,
+    "y" : 11,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_4",
+    "x" : 12,
+    "y" : 15,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 11,
+    "y" : 16,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 14,
+    "y" : 13,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 10,
+    "y" : 17,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 9,
+    "y" : 18,
+    "variant" : "left"
+  }, {
+    "type" : "trees_4",
+    "x" : 15,
+    "y" : 12,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_5",
+    "x" : 13,
+    "y" : 14,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 17,
+    "y" : 10,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 12,
+    "y" : 16,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 17,
+    "y" : 11,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_6",
+    "x" : 13,
+    "y" : 15,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_4",
+    "x" : 14,
+    "y" : 14,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 11,
+    "y" : 17,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 18,
+    "y" : 10,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 10,
+    "y" : 18,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_6",
+    "x" : 16,
+    "y" : 12,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_5",
+    "x" : 15,
+    "y" : 13,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 16,
+    "y" : 13,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 13,
+    "y" : 16,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 12,
+    "y" : 17,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 18,
+    "y" : 11,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 11,
+    "y" : 18,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 17,
+    "y" : 12,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_5",
+    "x" : 15,
+    "y" : 14,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_4",
+    "x" : 14,
+    "y" : 15,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_4",
+    "x" : 14,
+    "y" : 16,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 13,
+    "y" : 17,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 19,
+    "y" : 11
+  }, {
+    "type" : "trees_2",
+    "x" : 12,
+    "y" : 18
+  }, {
+    "type" : "trees_5",
+    "x" : 17,
+    "y" : 13
+  }, {
+    "type" : "trees_2",
+    "x" : 18,
+    "y" : 12,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 15,
+    "y" : 15,
+    "variant" : "right"
+  }, {
+    "type" : "trees_4",
+    "x" : 16,
+    "y" : 14,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 18,
+    "y" : 13,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_3",
+    "x" : 14,
+    "y" : 17,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_4",
+    "x" : 15,
+    "y" : 16,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 19,
+    "y" : 12,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 12,
+    "y" : 19,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_4",
+    "x" : 17,
+    "y" : 14,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_3",
+    "x" : 13,
+    "y" : 18,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_6",
+    "x" : 16,
+    "y" : 15,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_2",
+    "x" : 15,
+    "y" : 17,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_6",
+    "x" : 17,
+    "y" : 15,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 14,
+    "y" : 18,
+    "variant" : "left"
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 12,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 13,
+    "y" : 19,
+    "variant" : "left"
+  }, {
+    "type" : "trees_2",
+    "x" : 18,
+    "y" : 14,
+    "variant" : "bottom_middle"
+  }, {
+    "type" : "trees_1",
+    "x" : 19,
+    "y" : 13,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_6",
+    "x" : 16,
+    "y" : 16,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_4",
+    "x" : 17,
+    "y" : 16,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 16,
+    "y" : 17,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 13,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 14,
+    "y" : 19,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 15,
+    "y" : 18,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 18,
+    "y" : 15
+  }, {
+    "type" : "trees_2",
+    "x" : 19,
+    "y" : 14,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 14,
+    "variant" : "top_middle"
+  }, {
+    "type" : "trees_2",
+    "x" : 16,
+    "y" : 18,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 17,
+    "y" : 17,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 18,
+    "y" : 16,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 19,
+    "y" : 15,
+    "variant" : "bottom_right"
+  }, {
+    "type" : "trees_2",
+    "x" : 15,
+    "y" : 19
+  }, {
+    "type" : "trees_1",
+    "x" : 19,
+    "y" : 16,
+    "variant" : "bottom_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 15,
+    "variant" : "left"
+  }, {
+    "type" : "trees_2",
+    "x" : 17,
+    "y" : 18,
+    "variant" : "right"
+  }, {
+    "type" : "trees_2",
+    "x" : 18,
+    "y" : 17,
+    "variant" : "right"
+  }, {
+    "type" : "trees_1",
+    "x" : 18,
+    "y" : 18
+  }, {
+    "type" : "trees_1",
+    "x" : 19,
+    "y" : 17
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 16,
+    "variant" : "top_right"
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 17,
+    "variant" : "top_left"
+  }, {
+    "type" : "trees_1",
+    "x" : 20,
+    "y" : 18,
+    "variant" : "top_middle"
+  } ]
+}

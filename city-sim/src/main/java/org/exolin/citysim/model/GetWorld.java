78cf0cf1ed4b80fbea0c0ff4a38207c20eda1ab2
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-18 19:49:40 +0200

    add doc

--- city-sim/src/main/java/org/exolin/citysim/model/GetWorld.java
+++ city-sim/src/main/java/org/exolin/citysim/model/GetWorld.java
@@ -1,13 +1,24 @@
 package org.exolin.citysim.model;
 
 /**
+ * Allows accessing the current world
  *
  * @author Thomas
  */
 public interface GetWorld
 {
+    /**
+     * Return the current world
+     * @return current world
+     */
     World get();
     
+    /**
+     * Creates an instance where the world stays the same.
+     * 
+     * @param w world
+     * @return static
+     */
     static GetWorld ofStatic(World w)
     {
         return () -> w;

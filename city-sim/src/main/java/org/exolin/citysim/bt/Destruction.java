ac6611a9d828ad255debf44683af9f69d3d74fdc
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-20 22:32:24 +0200

    WIP for #74

--- city-sim/src/main/java/org/exolin/citysim/bt/Destruction.java
+++ city-sim/src/main/java/org/exolin/citysim/bt/Destruction.java
@@ -17,8 +17,36 @@ public class Destruction
             createAnimation("destruction/fire", 4, 500),
             1, Zones.destroyed, 0, BigDecimal.ZERO, Destruction::updateFire);
     
+    private static boolean spreadFire()
+    {
+        double r = Math.random();
+        System.out.println(r);
+        boolean spread = r < 0.1;
+        if(spread)
+            System.out.println("SPREAD FIRE");
+        return spread;
+    }
+    
     private static void updateFire(World w, Building b)
     {
-        //TODO
+        int x = b.getX();
+        int y = b.getY();
+        
+        maybeAddFire(w, x-1, y);
+        maybeAddFire(w, x+1, y);
+        maybeAddFire(w, x, y-1);
+        maybeAddFire(w, x, y+1);
+    }
+    
+    private static void maybeAddFire(World w, int x, int y)
+    {
+        int size = w.getGridSize();
+        if(x < 0 || x >= size || y < 0 || y <= size)
+            return;
+        
+        if(!spreadFire())
+            return;
+        
+        w.addBuilding(fire, x, y);
     }
 }

61fca870b85c3ab32f350b8468d0f4862215233d
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-21 18:04:43 +0200

    allow fire to jump streets
    part of #77

--- city-sim/src/main/java/org/exolin/citysim/bt/Destruction.java
+++ city-sim/src/main/java/org/exolin/citysim/bt/Destruction.java
@@ -3,9 +3,11 @@ package org.exolin.citysim.bt;
 import java.math.BigDecimal;
 import static org.exolin.citysim.bt.Buildings.createBuildingType;
 import static org.exolin.citysim.model.Animation.createAnimation;
+import org.exolin.citysim.model.Structure;
 import org.exolin.citysim.model.World;
 import org.exolin.citysim.model.building.Building;
 import org.exolin.citysim.model.building.BuildingType;
+import org.exolin.citysim.model.connection.Connection;
 
 /**
  *
@@ -17,39 +19,67 @@ public class Destruction
             createAnimation("destruction/fire", 4, 500),
             1, Zones.destroyed, 0, BigDecimal.ZERO, Destruction::updateFire);
     
-    private static boolean spreadFire(boolean high)
+    private static boolean spreadFire(double speed)
     {
         double r = Math.random();
-        double probability = high ? 0.0001 : 0.0001/3;
+        double probability = speed * 0.0001;
         boolean spread = r < probability;
         return spread;
     }
+    
+    private static final double NORMAL = 1;
+    private static final double DIAGONAL = 1/3.;
+    private static final double JUMP = 0.125;
     
     private static void updateFire(World w, Building b)
     {
         int x = b.getX();
         int y = b.getY();
         
-        maybeAddFire(w, x-1, y, true);
-        maybeAddFire(w, x+1, y, true);
-        maybeAddFire(w, x, y-1, true);
-        maybeAddFire(w, x, y+1, true);
+        boolean leftCanBurn = maybeAddFire(w, x-1, y, NORMAL);
+        boolean rightCanBurn = maybeAddFire(w, x+1, y, NORMAL);
+        boolean downCanBurn = maybeAddFire(w, x, y-1, NORMAL);
+        boolean upCanBurn = maybeAddFire(w, x, y+1, NORMAL);
         
-        maybeAddFire(w, x-1, y-1, false);
-        maybeAddFire(w, x-1, y+1, false);
-        maybeAddFire(w, x+1, y-1, false);
-        maybeAddFire(w, x+1, y+1, false);
+        maybeAddFire(w, x-1, y-1, DIAGONAL);
+        maybeAddFire(w, x-1, y+1, DIAGONAL);
+        maybeAddFire(w, x+1, y-1, DIAGONAL);
+        maybeAddFire(w, x+1, y+1, DIAGONAL);
+        
+        //only jump if there is something in the way
+        if(!leftCanBurn)
+            maybeAddFire(w, x-2, y, JUMP);
+        if(!rightCanBurn)
+            maybeAddFire(w, x+2, y, JUMP);
+        if(!downCanBurn)
+            maybeAddFire(w, x, y-2, JUMP);
+        if(!upCanBurn)
+            maybeAddFire(w, x, y+2, JUMP);
     }
     
-    private static void maybeAddFire(World w, int x, int y, boolean high)
+    /**
+     * 
+     * @param w
+     * @param x
+     * @param y
+     * @param speed
+     * @return {@true} if fire can spread here, regardless if it did now
+     */
+    private static boolean maybeAddFire(World w, int x, int y, double speed)
     {
         int size = w.getGridSize();
         if(x < 0 || x >= size || y < 0 || y >= size)
-            return;
+            return true;
         
-        if(!spreadFire(high))
-            return;
+        //don't put street/rail/water on fire
+        Structure b = w.getBuildingAt(x, y);
+        if(b instanceof Connection)
+            return false;
+        
+        if(!spreadFire(speed))
+            return true;
         
         w.addBuilding(fire, x, y);
+        return true;
     }
 }

9357b6d3764ce73194bcd3c90a49e2725df5912b
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-27 20:40:18 +0200

    add more fire animation
    closes #100

--- city-sim/src/main/java/org/exolin/citysim/model/fire/FireType.java
+++ city-sim/src/main/java/org/exolin/citysim/model/fire/FireType.java
@@ -18,7 +18,19 @@ public class FireType extends StructureType<Fire, FireType.Variant, FireParamete
             fireAnimation,
             fireAnimation.createRotated("destruction/fire2", 1),
             fireAnimation.createRotated("destruction/fire3", 2),
-            fireAnimation.createRotated("destruction/fire4", 3)
+            fireAnimation.createRotated("destruction/fire4", 3),
+            fireAnimation.createRotated("destruction/fire4", 4),
+            fireAnimation.createRotated("destruction/fire5", 5),
+            fireAnimation.createRotated("destruction/fire6", 6),
+            fireAnimation.createRotated("destruction/fire7", 7),
+            fireAnimation.createRotated("destruction/fire8", 8),
+            fireAnimation.createRotated("destruction/fire9", 9),
+            fireAnimation.createRotated("destruction/fire10", 10),
+            fireAnimation.createRotated("destruction/fire11", 11),
+            fireAnimation.createRotated("destruction/fire12", 12),
+            fireAnimation.createRotated("destruction/fire13", 13),
+            fireAnimation.createRotated("destruction/fire14", 14),
+            fireAnimation.createRotated("destruction/fire15", 15)
     ), 1);
     
     public enum Variant implements StructureVariant
@@ -26,7 +38,19 @@ public class FireType extends StructureType<Fire, FireType.Variant, FireParamete
         V1,
         V2,
         V3,
-        V4;
+        V4,
+        V5,
+        V6,
+        V7,
+        V8,
+        V9,
+        V10,
+        V11,
+        V12,
+        V13,
+        V14,
+        V15,
+        V16;
         
         private static final Variant[] values = values();
         

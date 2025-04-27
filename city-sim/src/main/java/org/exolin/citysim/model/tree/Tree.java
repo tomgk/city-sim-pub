cfdb2862e1f0a8eb955a92148d9c57d79fb9d60b
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-27 18:42:58 +0200

    make trees spread
    part of #97

--- city-sim/src/main/java/org/exolin/citysim/model/tree/Tree.java
+++ city-sim/src/main/java/org/exolin/citysim/model/tree/Tree.java
@@ -1,8 +1,11 @@
 package org.exolin.citysim.model.tree;
 
 import java.math.BigDecimal;
+import org.exolin.citysim.bt.Trees;
 import org.exolin.citysim.model.PlainStructureParameters;
 import org.exolin.citysim.model.Structure;
+import org.exolin.citysim.model.World;
+import org.exolin.citysim.utils.Utils;
 
 /**
  *
@@ -35,5 +38,45 @@ public class Tree extends Structure<Tree, TreeType, TreeType.Variant, PlainStruc
     public BigDecimal getTaxRevenue()
     {
         return BigDecimal.ZERO;
+    }
+    
+    private static final double PROBABILITY_GROWTH = 0.001;
+    private static final double PROBABILITY_SPREAD = 0.0001;
+
+    @Override
+    protected void updateAfterTick(World world, int ticks)
+    {
+        if(Math.random() < Utils.getProbabilityForTicks(PROBABILITY_GROWTH, ticks))
+        {
+            if(false)
+                world.removeBuildingAt(this);
+            //world.addBuilding(getType().plusOne(), getX(), getY(), getVariant());
+        }
+        
+        maybeGrow(world, getX()-1, getY(), ticks);
+        maybeGrow(world, getX()+1, getY(), ticks);
+        
+        maybeGrow(world, getX(), getY()-1, ticks);
+        maybeGrow(world, getX(), getY()+1, ticks);
+    }
+
+    private void maybeGrow(World world, int x, int y, int ticks)
+    {
+        if(x < 0 || y < 0)
+            return;
+        if(x >= world.getGridSize())
+            return;
+        if(y >= world.getGridSize())
+            return;
+        
+        if(world.getBuildingAt(x, y) != null)
+            return;
+        
+        double d = Math.random();
+        double p = Utils.getProbabilityForTicks(PROBABILITY_SPREAD, ticks);
+        if(d < p)
+        {
+            world.addBuilding(Trees.TREES.getFirst(), x, y);
+        }
     }
 }

ff094e227b88d219ce01bc7e8d2d8ecc1cd13c13
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-27 22:29:15 +0200

    WIP for #104

--- city-sim/src/main/java/org/exolin/citysim/utils/RandomUtils.java
+++ city-sim/src/main/java/org/exolin/citysim/utils/RandomUtils.java
@@ -15,16 +15,16 @@ public class RandomUtils
     
     public static int randInt(int max)
     {
-        return (int)(Math.random() * max);
+        return (int)(random() * max);
     }
 
     public static <T> T random(List<T> list)
     {
-        return list.get((int)(Math.random() * list.size()));
+        return list.get((int)(random() * list.size()));
     }
 
     public static boolean atLeast(double value)
     {
-        return Math.random() < value;
+        return random() < value;
     }
 }

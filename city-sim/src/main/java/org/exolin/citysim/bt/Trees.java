0986542226917d6c996c432ec9dc779565f5b866
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-28 21:54:10 +0200

    split up createTree
    for #99

--- city-sim/src/main/java/org/exolin/citysim/bt/Trees.java
+++ city-sim/src/main/java/org/exolin/citysim/bt/Trees.java
@@ -11,16 +11,21 @@ import org.exolin.citysim.utils.ImageUtils;
  */
 public class Trees
 {
-    private static TreeType createTree(int count, boolean alive)
+    private static TreeType createAliveTree(int count, boolean alive)
+    {
+        return new TreeType((!alive ? "dead_" : "")+"trees_"+count, ImageUtils.loadImage("trees/"+count), count, alive);
+    }
+    
+    private static TreeType createDeadTree(int count, boolean alive)
     {
         return new TreeType((!alive ? "dead_" : "")+"trees_"+count, ImageUtils.loadImage("trees/"+count), count, alive);
     }
     
     public static final List<TreeType> TREES = IntStream.range(1, 8)
-            .mapToObj(count -> createTree(count, true))
+            .mapToObj(count -> createAliveTree(count, true))
             .toList();
     
     public static final List<TreeType> DEAD_TREES = IntStream.range(1, 8)
-            .mapToObj(count -> createTree(count, false))
+            .mapToObj(count -> createDeadTree(count, false))
             .toList();
 }

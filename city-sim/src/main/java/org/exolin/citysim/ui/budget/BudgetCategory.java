22ab87667152eab676538be168ed21a3b2345bca
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-29 21:08:52 +0200

    add Vacant to Budget
    closes #112

--- city-sim/src/main/java/org/exolin/citysim/ui/budget/BudgetCategory.java
+++ city-sim/src/main/java/org/exolin/citysim/ui/budget/BudgetCategory.java
@@ -3,6 +3,8 @@ package org.exolin.citysim.ui.budget;
 import java.util.Objects;
 import org.exolin.citysim.model.StructureType;
 import org.exolin.citysim.model.building.BuildingType;
+import org.exolin.citysim.model.building.vacant.VacantType;
+import org.exolin.citysim.model.connection.cross.CrossConnectionType;
 import org.exolin.citysim.model.connection.regular.SelfConnectionType;
 import org.exolin.citysim.model.tree.TreeType;
 import org.exolin.citysim.model.zone.ZoneType;
@@ -24,9 +26,14 @@ public interface BudgetCategory
             return new ZoneCategory(a.getZoneType());
         else if(type instanceof SelfConnectionType s)
             return new SelfConnectionCategory(s);
+        else if(type instanceof CrossConnectionType s)
+            //TODO: ignores Y
+            return new SelfConnectionCategory(s.getXType());
         else if(type instanceof TreeType)
             return null;
         else if(type instanceof ZoneType)
+            return null;
+        else if(type instanceof VacantType)
             return null;
         else
         {

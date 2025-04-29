e568e3ed041fa79de3567c92bd5c6e37338376d2
T. G. <6294097+tomgk@users.noreply.github.com>
2025-04-29 21:01:01 +0200

    Update EmptyStructureParametersTest.java

--- city-sim/src/test/java/org/exolin/citysim/model/EmptyStructureParametersTest.java
+++ city-sim/src/test/java/org/exolin/citysim/model/EmptyStructureParametersTest.java
@@ -5,7 +5,6 @@ import java.lang.reflect.InvocationTargetException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**

3317fcec7d1eb6130c1be0ecee7efe9592a3a2dd
T. G. <6294097+tomgk@users.noreply.github.com>
2025-05-03 19:36:05 +0200

    more tests for PropertyWriter

--- city-sim/src/test/java/org/exolin/citysim/utils/PropertyWriterTest.java
+++ city-sim/src/test/java/org/exolin/citysim/utils/PropertyWriterTest.java
@@ -17,4 +17,44 @@ public class PropertyWriterTest
         
         assertEquals("test[]", p.toString());
     }
+    
+    @Test
+    public void testAdd_int()
+    {
+        PropertyWriter p = new PropertyWriter("test");
+        p.add("value", 1);
+        p.finish();
+        
+        assertEquals("test[value=1]", p.toString());
+    }
+    
+    @Test
+    public void testAdd_String()
+    {
+        PropertyWriter p = new PropertyWriter("test");
+        p.add("name", "abc");
+        p.finish();
+        
+        assertEquals("test[name=abc]", p.toString());
+    }
+    
+    @Test
+    public void testAddOptional_Boolean_false()
+    {
+        PropertyWriter p = new PropertyWriter("test");
+        p.addOptional("advanced", false);
+        p.finish();
+        
+        assertEquals("test[]", p.toString());
+    }
+    
+    @Test
+    public void testAddOptional_Boolean_true()
+    {
+        PropertyWriter p = new PropertyWriter("test");
+        p.addOptional("advanced", true);
+        p.finish();
+        
+        assertEquals("test[advanced=true]", p.toString());
+    }
 }

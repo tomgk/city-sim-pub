568f20e19fdf2e2f4bbde87fb3a02655bb1eae40
T. G. <6294097+tomgk@users.noreply.github.com>
2025-05-22 20:24:49 +0200

    refa

--- city-sim/src/main/java/org/exolin/citysim/model/connection/Connection.java
+++ city-sim/src/main/java/org/exolin/citysim/model/connection/Connection.java
@@ -12,11 +12,13 @@ import org.exolin.citysim.model.StructureVariant;
  * @param <E>
  * @param <D>
  */
-public abstract class Connection<B extends Connection<B, T, E, D>, T extends ConnectionType<B, T, E, D>, E extends StructureVariant, D extends StructureParameters<D>> extends Structure<B, T, E, D>
+public abstract class Connection<
+        B extends Connection<B, T, E, D>,
+        T extends ConnectionType<B, T, E, D>,
+        E extends StructureVariant, D extends StructureParameters<D>> extends Structure<B, T, E, D>
 {
     public Connection(T type, int x, int y, E variant, D data)
     {
         super(type, x, y, variant, data);
     }
-    
 }

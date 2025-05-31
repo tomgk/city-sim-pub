024a465e03dbf0fbba901bbfdd048ad17e590bf5
T. G. <6294097+tomgk@users.noreply.github.com>
2025-05-31 21:08:57 +0200

    Create StructureVariantTest.java
    for #157

---
+++ city-sim/src/test/java/org/exolin/citysim/model/StructureVariantTest.java
@@ -0,0 +1,36 @@
+package org.exolin.citysim.model;
+
+import java.util.EnumSet;
+import java.util.Set;
+import org.exolin.citysim.model.connection.regular.ConnectVariant;
+import static org.exolin.citysim.model.connection.regular.ConnectVariant.CONNECT_X;
+import static org.exolin.citysim.model.connection.regular.ConnectVariant.CONNECT_Y;
+import org.exolin.citysim.model.connection.regular.ConnectionVariant;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Thomas
+ */
+public class StructureVariantTest
+{
+    @Test
+    public void testConnectVariant_Values()
+    {
+        Set<? extends StructureVariant> values = StructureVariant.getValues(ConnectVariant.class);
+        assertEquals(EnumSet.of(CONNECT_X, CONNECT_Y), values);
+    }
+    
+    @Test
+    public void testConnectVariant_Values_()
+    {
+        try{
+            StructureVariant.getValues(ConnectionVariant.class);
+            fail();
+        }catch(UnsupportedOperationException e){
+            
+        }
+    }
+}

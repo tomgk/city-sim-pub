f8396068d6b375c3a7f0453a9276531c08157224
T. G. <6294097+tomgk@users.noreply.github.com>
2025-05-31 22:43:40 +0200

    more for #157

--- city-sim/src/test/java/org/exolin/citysim/model/StructureVariantTest.java
+++ city-sim/src/test/java/org/exolin/citysim/model/StructureVariantTest.java
@@ -7,6 +7,8 @@ import static java.awt.BorderLayout.WEST;
 import java.util.EnumSet;
 import java.util.Set;
 import org.exolin.citysim.bt.StructureTypes;
+import org.exolin.citysim.model.building.BuildingType;
+import org.exolin.citysim.model.connection.cross.CrossConnectionType;
 import org.exolin.citysim.model.connection.regular.ConnectVariant;
 import static org.exolin.citysim.model.connection.regular.ConnectVariant.CONNECT_X;
 import static org.exolin.citysim.model.connection.regular.ConnectVariant.CONNECT_Y;
@@ -15,13 +17,16 @@ import static org.exolin.citysim.model.connection.regular.Curve.CURVE_1;
 import static org.exolin.citysim.model.connection.regular.Curve.CURVE_2;
 import static org.exolin.citysim.model.connection.regular.Curve.CURVE_3;
 import static org.exolin.citysim.model.connection.regular.Curve.CURVE_4;
+import org.exolin.citysim.model.connection.regular.SelfConnectionType;
 import static org.exolin.citysim.model.connection.regular.TIntersection.T_INTERSECTION_1;
 import static org.exolin.citysim.model.connection.regular.TIntersection.T_INTERSECTION_2;
 import static org.exolin.citysim.model.connection.regular.TIntersection.T_INTERSECTION_3;
 import static org.exolin.citysim.model.connection.regular.TIntersection.T_INTERSECTION_4;
 import static org.exolin.citysim.model.connection.regular.Unconnected.UNCONNECTED;
 import static org.exolin.citysim.model.connection.regular.XIntersection.X_INTERSECTION;
+import org.exolin.citysim.model.zone.ZoneType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -76,5 +81,37 @@ public class StructureVariantTest
     {
         Set<? extends StructureVariant> values = StructureVariant.getValues(ConnectionVariant.class);
         assertEquals(EnumSet.of(CONNECT_X, CONNECT_Y), values);
+    }
+    
+    @Test
+    public void testZoneType()
+    {
+        Set<? extends StructureVariant> values = StructureVariant.getValues(ZoneType.Variant.class);
+        assertEquals(Set.of(ZoneType.Variant.DEFAULT), values);
+    }
+    
+    @Test
+    public void testBuildingType()
+    {
+        Set<? extends StructureVariant> values = StructureVariant.getValues(BuildingType.Variant.class);
+        assertEquals(Set.of(BuildingType.Variant.DEFAULT), values);
+    }
+    
+    @Test
+    @Disabled
+    public void testSelfConnectionType()
+    {
+        Set<? extends StructureVariant> values = StructureVariant.getValues(StructureType.getStructureVariantClass(SelfConnectionType.class));
+        Set expected = Set.of(
+                CONNECT_X, CONNECT_Y, X_INTERSECTION, CURVE_1, CURVE_2, CURVE_3, CURVE_4, T_INTERSECTION_1, T_INTERSECTION_2, T_INTERSECTION_3, T_INTERSECTION_4, NORTH, WEST, SOUTH, EAST, UNCONNECTED
+        );
+        assertEquals(expected, values);
+    }
+    
+    @Test
+    public void testCrossConnectionType()
+    {
+        Set<? extends StructureVariant> values = StructureVariant.getValues(CrossConnectionType.Variant.class);
+        assertEquals(Set.of(CrossConnectionType.Variant.DEFAULT), values);
     }
 }

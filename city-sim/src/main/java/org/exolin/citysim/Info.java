3402fc2e47f97a1b942137dc949e085d7c6fb67f
T. G. <6294097+tomgk@users.noreply.github.com>
2025-06-01 18:21:59 +0200

    add classes to Info

--- city-sim/src/main/java/org/exolin/citysim/Info.java
+++ city-sim/src/main/java/org/exolin/citysim/Info.java
@@ -2,8 +2,8 @@ package org.exolin.citysim;
 
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.exolin.citysim.bt.StructureTypes;
-import org.exolin.citysim.bt.buildings.Plants;
 import org.exolin.citysim.model.StructureType;
 import org.exolin.citysim.model.StructureVariant;
 
@@ -20,8 +20,26 @@ public class Info
     
     public static void main(String[] args)
     {
+        System.out.println("--- C L A S S E S ---");
+        
+        StructureType.types()
+                .stream()
+                .map(t -> t.getClass())
+                .distinct()
+                .forEach(t -> {
+                    System.out.println("==== "+t.getSimpleName()+" =====");
+                    Set<? extends StructureVariant> variants = StructureVariant.getValues((Class)t);
+                    System.out.println("Variants: "+variants.stream()
+                            .map(v -> v.name())
+                            .collect(Collectors.joining(", "))
+                    );
+                });
+        
+        System.out.println("--- T Y P E S ---");
+        
         StructureType.types().forEach((StructureType s) -> {
             System.out.println("==== "+s.getName()+" =====");
+            System.out.println("Type: "+s.getClass().getSimpleName());
             
             Set<? extends StructureVariant> variants;
             try{
@@ -32,8 +50,14 @@ public class Info
             }
             if(variants != null)
             {
-                if(variants.size() == 1)
-                    System.out.println("Cost: "+s.getBuildingCost(variants.iterator().next()));
+                Set<Integer> diffCosts = variants.stream()
+                        .map(v -> s.getBuildingCost(v))
+                        .collect(Collectors.toSet());
+                
+                if(diffCosts.size() == 1)
+                {
+                    System.out.println("Cost: "+diffCosts.iterator().next());
+                }
                 else
                 {
                     System.out.println("Cost:");

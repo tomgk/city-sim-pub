b575080fb2ee4bd235f90ea13b7b66b0747b3800
T. G. <6294097+tomgk@users.noreply.github.com>
2025-06-10 20:27:15 +0200

    Split up DebugTableModelRun
    closes #174

--- city-sim/src/main/java/org/exolin/citysim/ui/debug/DebugTableModelRun.java
+++ city-sim/src/main/java/org/exolin/citysim/ui/debug/DebugTableModelRun.java
@@ -23,152 +23,152 @@ public class DebugTableModelRun
         System.out.println(name+"="+value+" ["+value.getClass().getSimpleName()+"]");
     }
     
+    private static class BoolVal implements Value<Boolean>
+    {
+        private final String name;
+        private boolean val;
+
+        public BoolVal(String name)
+        {
+            this.name = name;
+        }
+
+        @Override
+        public Boolean get()
+        {
+            return val;
+        }
+
+        @Override
+        public void set(Boolean value)
+        {
+            this.val = value;
+            output(name, value);
+        }
+    }
+
+    private static class StrVal implements Value<String>
+    {
+        private final String name;
+        private String val;
+
+        public StrVal(String name, String value)
+        {
+            this.name = name;
+            this.val = value;
+        }
+
+        @Override
+        public String get()
+        {
+            return val;
+        }
+
+        @Override
+        public void set(String value)
+        {
+            this.val = value;
+            output(name, value);
+        }
+    }
+
+    private static class IntVal implements Value<Integer>
+    {
+        private final String name;
+        private int val;
+
+        public IntVal(String name, int value)
+        {
+            this.name = name;
+            this.val = value;
+        }
+
+        @Override
+        public Integer get()
+        {
+            return val;
+        }
+
+        @Override
+        public void set(Integer value)
+        {
+            this.val = value;
+            output(name, value);
+        }
+    }
+
+    private static class ReadonlyIntVal implements ReadonlyValue<Integer>
+    {
+        private final int val;
+
+        public ReadonlyIntVal(int value)
+        {
+            this.val = value;
+        }
+
+        @Override
+        public Integer get()
+        {
+            return val;
+        }
+    }
+
+    private static class BigDecVal implements Value<BigDecimal>
+    {
+        private final String name;
+        private BigDecimal val;
+
+        public BigDecVal(String name, BigDecimal value)
+        {
+            this.name = name;
+            this.val = value;
+        }
+
+        @Override
+        public BigDecimal get()
+        {
+            return val;
+        }
+
+        @Override
+        public void set(BigDecimal value)
+        {
+            this.val = value;
+            output(name, value);
+        }
+    }
+
+    enum YesNo{YES, NO, MAYBE}
+
+    private static class EnumVal<E extends Enum<E>> extends EnumValue<E>
+    {
+        private final String name;
+        private E val;
+
+        public EnumVal(Class<E> type, String name, E value)
+        {
+            super(type);
+            this.name = name;
+            this.val = value;
+        }
+
+        @Override
+        public E get()
+        {
+            return val;
+        }
+
+        @Override
+        public void set(E value)
+        {
+            this.val = value;
+            output(name, value);
+        }
+    }
+        
     public static void main1(String[] args)
     {
         List<Map.Entry<String, Value<?>>> values = new ArrayList<>();
-        
-        class BoolVal implements Value<Boolean>
-        {
-            private final String name;
-            private boolean val;
-
-            public BoolVal(String name)
-            {
-                this.name = name;
-            }
-            
-            @Override
-            public Boolean get()
-            {
-                return val;
-            }
-
-            @Override
-            public void set(Boolean value)
-            {
-                this.val = value;
-                output(name, value);
-            }
-        }
-        
-        class StrVal implements Value<String>
-        {
-            private final String name;
-            private String val;
-
-            public StrVal(String name, String value)
-            {
-                this.name = name;
-                this.val = value;
-            }
-            
-            @Override
-            public String get()
-            {
-                return val;
-            }
-
-            @Override
-            public void set(String value)
-            {
-                this.val = value;
-                output(name, value);
-            }
-        }
-        
-        class IntVal implements Value<Integer>
-        {
-            private final String name;
-            private int val;
-
-            public IntVal(String name, int value)
-            {
-                this.name = name;
-                this.val = value;
-            }
-            
-            @Override
-            public Integer get()
-            {
-                return val;
-            }
-
-            @Override
-            public void set(Integer value)
-            {
-                this.val = value;
-                output(name, value);
-            }
-        }
-        
-        class ReadonlyIntVal implements ReadonlyValue<Integer>
-        {
-            private final int val;
-
-            public ReadonlyIntVal(int value)
-            {
-                this.val = value;
-            }
-            
-            @Override
-            public Integer get()
-            {
-                return val;
-            }
-        }
-        
-        class BigDecVal implements Value<BigDecimal>
-        {
-            private final String name;
-            private BigDecimal val;
-
-            public BigDecVal(String name, BigDecimal value)
-            {
-                this.name = name;
-                this.val = value;
-            }
-            
-            @Override
-            public BigDecimal get()
-            {
-                return val;
-            }
-
-            @Override
-            public void set(BigDecimal value)
-            {
-                this.val = value;
-                output(name, value);
-            }
-        }
-        
-        enum YesNo{YES, NO, MAYBE}
-        
-        class EnumVal<E extends Enum<E>> extends EnumValue<E>
-        {
-            private final String name;
-            private E val;
-
-            public EnumVal(Class<E> type, String name, E value)
-            {
-                super(type);
-                this.name = name;
-                this.val = value;
-            }
-            
-            @Override
-            public E get()
-            {
-                return val;
-            }
-
-            @Override
-            public void set(E value)
-            {
-                this.val = value;
-                output(name, value);
-            }
-        }
         
         values.add(new AbstractMap.SimpleImmutableEntry<>("cityName", new StrVal("cityName", "TestVille")));
         values.add(new AbstractMap.SimpleImmutableEntry<>("needElectricity", new BoolVal("needElectricity")));

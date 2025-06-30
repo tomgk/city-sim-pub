bed77ab21fd33b66ebba1052a56102beb43a58aa
T. G. <6294097+tomgk@users.noreply.github.com>
2025-06-30 16:34:00 +0200

    add InfoResourcesTest
    closes #202

--- city-sim/src/test/java/org/exolin/citysim/InfoResourcesTest.java
+++ city-sim/src/test/java/org/exolin/citysim/InfoResourcesTest.java
@@ -27,7 +27,7 @@ import org.junit.jupiter.params.provider.MethodSource;
  *
  * @author Thomas
  */
-public class InfoTypeTest
+public class InfoResourcesTest
 {
     static
     {
@@ -39,148 +39,173 @@ public class InfoTypeTest
                     BusinessBuildings.car_cinema,
                     """
                     ==== business_car-cinema =====
-                    Type: BuildingType
-                    Cost: -
-                    Size: 3x3
-                    Zone: zone_business
+                    Image:
+                     - DEFAULT: business/car-cinema, 9 f, 1000 ms/f
+                     - ROTATED: business/car-cinema, 9 f, 1000 ms/f
                     """
             ),
             Map.entry(
                     Plants.get(PlantTypeType.TREE, 3),
                     """
                     ==== trees_3 =====
-                    Type: PlantType
-                    Cost: 9
-                    Size: 1x1
-                    Count: 3
-                    Type: TREE
-                    IsAlive: true
+                    Image:
+                     - DEFAULT: trees_3
+                     - LEFT: trees_3@LEFT
+                     - RIGHT: trees_3@RIGHT
+                     - TOP_LEFT: trees_3@TOP_LEFT
+                     - TOP_MIDDLE: trees_3@TOP_MIDDLE
+                     - TOP_RIGHT: trees_3@TOP_RIGHT
+                     - BOTTOM_LEFT: trees_3@BOTTOM_LEFT
+                     - BOTTOM_MIDDLE: trees_3@BOTTOM_MIDDLE
+                     - BOTTOM_RIGHT: trees_3@BOTTOM_RIGHT
                     """
             ),
             Map.entry(
                     PowerPlants.fusion_plant,
                     """
                     ==== Fusion Power =====
-                    Type: BuildingType
-                    Cost: 40000
-                    Size: 4x4
-                    Zone: zone_plants
-                    megaWatt: 2500 MW
+                    Image:
+                     - DEFAULT: plants/fusion_plant
+                     - ROTATED: plants/fusion_plant
                     """
             ),
             Map.entry(
                     PowerPlants.hydro_plant,
                     """
                     ==== Hydro Power =====
-                    Type: BuildingType
-                    Cost: 400
-                    Size: 1x1
-                    Zone: zone_plants
-                    megaWatt: 20 MW
+                    Image:
+                     - DEFAULT: plants/hydro_x
+                     - ROTATED: plants/hydro_y
                     """
             ),
             Map.entry(
                     Vacants.abandoned_middle_2,
                     """
                     ==== destruction/abandoned_middle_2 =====
-                    Type: VacantType
-                    Cost: -
-                    Size: 2x2
+                    Image: destruction/abandoned_middle_2
                     """
             ),
             Map.entry(
                     Parks.zoo,
                     """
                     ==== parks_zoo =====
-                    Type: BuildingType
-                    Cost: 100
-                    Size: 4x4
-                    Zone: zone_parks
+                    Image:
+                     - DEFAULT: parks/zoo
+                     - ROTATED: parks/zoo
                     """
             ),
             Map.entry(
                     CrossConnections.RAIL_STREET,
                     """
                     ==== rail_street =====
-                    Type: CrossConnectionType
-                    Cost: -
-                    Size: 1x1
-                    X-Type: rail
-                    Y-Type: street
+                    Image: cross_connection/rail_street
                     """
             ),
             Map.entry(
                     CrossConnections.WATER_CIRCUIT,
                     """
                     ==== water_circuit =====
-                    Type: CrossConnectionType
-                    Cost: -
-                    Size: 1x1
-                    X-Type: water
-                    Y-Type: circuit
+                    Image: cross_connection/water_circuit
                     """
             ),
             Map.entry(
                     SelfConnections.street,
                     """
                     ==== street =====
-                    Type: SelfConnectionType
-                    Cost: 10
-                    Size: 1x1
+                    DefaultImage: CONNECT_X
+                    Image:
+                     - CONNECT_X: street/street_1
+                     - CONNECT_Y: street/street_2
+                     - X_INTERSECTION: street/street_x_intersection, 5 f, 1000 ms/f
+                     - CURVE_1: street/street_curve_1
+                     - CURVE_2: street/street_curve_2
+                     - CURVE_3: street/street_curve_3
+                     - CURVE_4: street/street_curve_4
+                     - T_INTERSECTION_1: street/street_t_1
+                     - T_INTERSECTION_2: street/street_t_2
+                     - T_INTERSECTION_3: street/street_t_3
+                     - T_INTERSECTION_4: street/street_t_4
+                     - NORTH: street/street_1
+                     - WEST: street/street_2
+                     - SOUTH: street/street_1
+                     - EAST: street/street_2
+                     - UNCONNECTED: street/street_1
                     """
             ),
             Map.entry(
                     SelfConnections.rail,
                     """
                     ==== rail =====
-                    Type: SelfConnectionType
-                    Cost: 25
-                    Size: 1x1
+                    DefaultImage: CONNECT_X
+                    Image:
+                     - CONNECT_X: rail/rail_1
+                     - CONNECT_Y: rail/rail_2
+                     - X_INTERSECTION: rail/rail_x_intersection
+                     - CURVE_1: rail/rail_curve_1
+                     - CURVE_2: rail/rail_curve_2
+                     - CURVE_3: rail/rail_curve_3
+                     - CURVE_4: rail/rail_curve_4
+                     - T_INTERSECTION_1: rail/rail_t_1
+                     - T_INTERSECTION_2: rail/rail_t_2
+                     - T_INTERSECTION_3: rail/rail_t_3
+                     - T_INTERSECTION_4: rail/rail_t_4
+                     - NORTH: rail/rail_1
+                     - WEST: rail/rail_2
+                     - SOUTH: rail/rail_1
+                     - EAST: rail/rail_2
+                     - UNCONNECTED: rail/rail_1
                     """
             ),
             Map.entry(
                     FireType.fire,
                     """
                     ==== fire =====
-                    Type: FireType
-                    Cost: -
-                    Size: 1x1
+                    DefaultImage: V1
+                    Image:
+                     - V1: destruction/fire, 16 f, 500 ms/f
+                     - V2: destruction/fire2, 16 f, 500 ms/f
+                     - V3: destruction/fire3, 16 f, 500 ms/f
+                     - V4: destruction/fire4, 16 f, 500 ms/f
+                     - V5: destruction/fire5, 16 f, 500 ms/f
+                     - V6: destruction/fire6, 16 f, 500 ms/f
+                     - V7: destruction/fire7, 16 f, 500 ms/f
+                     - V8: destruction/fire8, 16 f, 500 ms/f
+                     - V9: destruction/fire9, 16 f, 500 ms/f
+                     - V10: destruction/fire10, 16 f, 500 ms/f
+                     - V11: destruction/fire11, 16 f, 500 ms/f
+                     - V12: destruction/fire12, 16 f, 500 ms/f
+                     - V13: destruction/fire13, 16 f, 500 ms/f
+                     - V14: destruction/fire14, 16 f, 500 ms/f
+                     - V15: destruction/fire15, 16 f, 500 ms/f
+                     - V16: destruction/fire16, 16 f, 500 ms/f
                     """
             ),
             Map.entry(
                     Zones.business,
                     """
                     ==== zone_business =====
-                    Type: ZoneType
-                    Cost: 5
-                    Size: 1x1
+                    Image: zone/business
                     """
             ),
             Map.entry(
                     Zones.low_business,
                     """
                     ==== zone_business_low =====
-                    Type: ZoneType
-                    Cost: 5
-                    Size: 1x1
+                    Image: zone/business_low
                     """
             ),
             Map.entry(
                     Zones.parks,
                     """
                     ==== zone_parks =====
-                    Type: ZoneType
-                    Cost: -
-                    Size: 1x1
+                    Image: zone/special
                     """
             ),
             Map.entry(
                     Zones.plants,
                     """
                     ==== zone_plants =====
-                    Type: ZoneType
-                    Cost: -
-                    Size: 1x1
+                    Image: zone/special
                     """
             )
     );
@@ -199,13 +224,13 @@ public class InfoTypeTest
     
     @ParameterizedTest
     @MethodSource("types")
-    public void testTypeInfo(StructureType<?, ?, ?> type)
+    public void testResourceInfo(StructureType<?, ?, ?> type)
     {
         String expected = EXPECTED_TYPEINFO.get(type);
         if(expected == null)
             return;
         
-        String actual = StringPrinter.print(out -> InfoType.typeInfo(out, type));
+        String actual = StringPrinter.print(out -> InfoResources.resourceInfo(out, type));
         assertEquals(expected, actual);
     }
     
